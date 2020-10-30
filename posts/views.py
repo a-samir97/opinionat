ad4533c7d0ae25d261c8b38baca6c2edd2b38c63
a-samir97 <a.samir9710@gmail.com>
2020-10-30 15:33:05 +0200

    starting in post apps

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from rest_framework import status 
+
+
+
+class PostAPIView(APIView):
+    """
+    """
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def delete(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+
+class CommentAPIView(APIView):
+    """
+    """
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def delete(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+
+class UpvoteAPIView(APIView):
+    """
+    """
+    def post(self, request):
+        pass
+
+class DownvoteAPIView(APIView):
+    """
+    """
+    def post(self, request):
+        pass
