7f3f189b3e1412337313c3226bfdd436dbba9cb6
a-samir97 <a.samir9710@gmail.com>
2020-10-30 11:17:01 +0200

    edit secret key in settings file;

--- opinionat/settings.py
+++ opinionat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'htaq=$32hf*m3bz(s806=42gofv@!0w^+=--2ire=hrmt3(m3-'
+SECRET_KEY = ''#'htaq=$32hf*m3bz(s806=42gofv@!0w^+=--2ire=hrmt3(m3-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
